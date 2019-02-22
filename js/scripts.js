6316144a6d70ca729d71ac16a331a5b08e692abe
skuzaman <anthony.ahere@gmail.com>
2019-02-22 18:30:27 +0300

    first batch commits

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
-function smt() {
-var question1=document.getElementById("tag1").value;
-var question2=document.getElementById("tag2").value;
-var question3=document.getElementById("tag3").value;
-var question4=document.getElementById("tag4").value;
+$(document).ready(function() {
+  $(".test").submit(function(event){
+    event.preventDefault();
+    var question1 = $("input:radio[name=answer1]:checked").val();
+    var question2 = $("input:radio[name=answer2]:checked").val();
+    var question3 = $("input:radio[name=answer3]:checked").val();
+    var question4 = $("input:radio[name=answer4]:checked").val();
 
-var val1= parseInt(question1);
-var val2 = parseInt(question2);
-var val3 = parseInt(question3);
-var val4 = parseInt(question4);
-var add = (val1 + val2 + val3);
+    var val1 = parseInt(question1);
+    var val2 = parseInt(question2);
+    var val3 = parseInt(question3);
+    var val4 = parseInt(question4);
+    var add = (val1 + val2 + val3 + val4);
 
-if (val1==val2 && val2==val3 && val4==val4) {
-  alert("your score is 40")
-  }
-else if (val1==val2 && val2==val3 && val3!=val4){
-  alert("your score is 30")
- }
-else if (val1==val4 && val2==val3 && val3!=val1){
-   alert("your score is 30")
-  }
-else if (val1==val2 && val2==val4 && val3!=val2){
-    alert("your score is 30")
-  }
-else if (val1==val3 && val2==val3 && val2!=val4){
-    alert("your score is 30")
- }
-}
+    window.open("index2.html", "_self");
+  });
+});
+
+var next = function(){
+    window.open("index2.html", "_self");
+};
+
+
+var retake = function(){
+  window.open("index.html", "_self");
+};
+
+var submit = function(){
+    alert()
+};
